c19eaf79b1b454e2553e968e36cc745b6decb890
Juan Espiritu <72969241@continental.edu.pe>
2022-07-29 22:23:19 -0500

    Titulo cambiado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container py-5 text-center">
-        <h1>Hola mundo!</h1>
+        <h1>Crud con Laravel</h1>
         <form action="{{route('client.index')}}">
             <button type="submit" class="btn btn-primary" >Ver Clientes</button>
         </form>
